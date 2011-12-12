0cf6c2ffc68a6ac3748ee5b1aa33cf4d3a1a516d
Justin Morris <justin@pixelbloom.com>
2011-12-13 02:12:41 +1100

    BuildQueue class is iterable…kinda.

--- henchman/lib/buildqueue.py
+++ henchman/lib/buildqueue.py
@@ -7,12 +7,24 @@ class BuildQueue(object):
 
   def __init__(self):
     self._queue = []
+    self.current_item = 0
 
   def __len__(self):
     return len(self._queue)
+
+  def __iter__(self):
+    return self
+
+  def next(self):
+    if (self.current_item == len(self._queue)):
+        raise StopIteration
+    else:
+        data = self._queue[self.current_item]
+        self.current_item += 1
+        return data
 
   def append(self, id):
     """
     Create a new Minion instance for `id` and append it to the internal queue list.
     """
-    self._queue.append(Minion(build_id=build_id))
+    self._queue.append(Minion(id=id))
