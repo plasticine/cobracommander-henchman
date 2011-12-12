6829cec387e3cb62940056aaf7f1be5e53e0ce18
Justin Morris <justin@pixelbloom.com>
2011-12-13 00:50:09 +1100

    Remove old code.

--- views/minion.py
+++
@@ -1,7 +0,0 @@
-from werkzeug.wrappers import Response
-
-def new(server, request):
-    """
-    Accepts POST requests containing the id of the build to spawn a minion for.
-    """
-    return Response('new')
