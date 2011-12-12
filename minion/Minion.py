b8d2036eec43ad92a295efdb02300505e6592a73
Justin Morris <justin@pixelbloom.com>
2011-12-13 00:51:47 +1100

    Minion should accept build_id argument.

--- minion/Minion.py
+++ minion/Minion.py
@@ -5,8 +5,8 @@ class Minion(object):
     Each Minion is a worker process that represents a build.
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, build_id):
+        self.build_id = build_id
 
     def start(self):
         pass
