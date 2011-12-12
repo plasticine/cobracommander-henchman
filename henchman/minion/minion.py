de23d85f89df1ff61460f2027412a1c953eb59bb
Justin Morris <justin@pixelbloom.com>
2011-12-13 02:12:18 +1100

    __repr__ and status methods on Minion

--- henchman/minion/minion.py
+++ henchman/minion/minion.py
@@ -5,8 +5,17 @@ class Minion(object):
     Each Minion is a worker process that represents a build.
     """
 
-    def __init__(self, build_id):
-        self.build_id = build_id
+    def __init__(self, id):
+        self.build_id = id
+
+    def __repr__(self):
+        return "<%s build_id=%s status=%s>" % ("Minion", self.build_id, self.status())
+
+    def status(self):
+        """
+        Returns the status of the minion
+        """
+        return "waiting"
 
     def start(self):
         pass
