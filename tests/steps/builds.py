d073257e96da5cb8a18ecc09db296cecbc4c4121
Justin Morris <justin@pixelbloom.com>
2011-12-13 00:50:59 +1100

    Alter response body.

--- tests/steps/builds.py
+++ tests/steps/builds.py
@@ -5,7 +5,7 @@ from nose.tools import ok_, eq_, raises, timed
 @when(u'Henchman recieves a POST request to create a build')
 def step(context):
     r = requests.post('http://localhost:9999/builds/new', data={'id':1})
-    eq_(r.content, "New build with id:1 added to queue.")
+    eq_(r.content, "OK")
 
 @then(u'Henchman creates a new build and appends it to the build-queue')
 def step(context):
