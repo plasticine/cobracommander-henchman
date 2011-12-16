55e6ac185e3caf7b347f86a0b1f656e678b6a957
Justin Morris <justin@pixelbloom.com>
2011-12-16 20:37:43 +1100

    Start writing Git tests.

--- henchman/minion/lib/Git.py
+++ henchman/minion/lib/Git.py
@@ -1,20 +1,33 @@
-
+from git import *
+from django.conf import settings
 
 class Git(object):
-    """
-    Handle cloning a project remote repo.
+  """
+  Handle cloning a project remote repo.
 
-    Creates a local cache of the remote for the first run and then use this to
-    create additional copies for each of the project build targets on first run
-    of each of them.
+  Creates a local cache of the remote for the first run and then use this to
+  create additional copies for each of the project build targets on first run
+  of each of them.
 
-    This allows for quicker creation of new build-targets for the project (we
-    can just update & copy then just cached repo and then `reset --hard` onto
-    the required target within the copy, saving us from a full git clone each
-    time.) It also means that we can properly sandbox the codebase for each
-    build-target from the others — allowing for the possibility of having two
-    build-targets for the same project executing in parallel.
-    """
+  This allows for quicker creation of new build-targets for the project (we
+  can just update & copy then just cached repo and then `reset --hard` onto
+  the required target within the copy, saving us from a full git clone each
+  time.) It also means that we can properly sandbox the codebase for each
+  build-target from the others — allowing for the possibility of having two
+  build-targets for the same project executing in parallel.
+  """
 
-    def __init__(self):
-        pass
+  def __init__(self, url, uuid):
+    self.url = url
+    self.uuid = uuid
+    self.path = os.path.join(settings.BUILD_ROOT, self.uuid)
+
+  def clone(self):
+    pass
+
+  def rebase(self):
+    git = repo.git
+    git.reset('head', hard=True)
+
+  def update(self, refspec):
+    pass
