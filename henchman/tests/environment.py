a7886af13ab1bc2631d857e7a48d2b37bfc10646
Justin Morris <justin@pixelbloom.com>
2011-12-16 20:37:33 +1100

    Fix test server to use new fangled henchman server.

--- henchman/tests/environment.py
+++ henchman/tests/environment.py
@@ -1,19 +1,23 @@
 import gevent
 from gevent.monkey import patch_all; patch_all()
 from multiprocessing import Process
-from ..server import server, run_server
-from ..views.socketio import SocketIOHandler
+
+from henchman import setup_environment
+setup_environment()
+
+from henchman.lib.server import Server, run
+from henchman.views.socketio import SocketIOHandler
 
 def before_all(context):
     # run the Henchman server
     # we need to wrap the serve_forever call in another process here to ensure
     # that it does not block the test execution yet is available to serve
     # testing requests.
-    context.server = run_server(server.application, 'localhost', 9999)
+    _server = Server()
+    context.server = run(_server.application, 'localhost', 9999)
     context.process = Process(target=context.server.serve_forever)
     context.process.start()
 
 def after_all(context):
-    SocketIOHandler.cleanup()
     context.server.kill()
     context.process.terminate()
