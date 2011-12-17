f39340565d15c664660ec45efe8ce6526a07ab41
Justin Morris <justin@pixelbloom.com>
2011-12-17 11:23:21 +1100

    Snakfile

--- henchman/minion/lib/Snakefile.py
+++ henchman/minion/lib/Snakefile.py
@@ -1,4 +1,5 @@
-
+from os import path
+from .step import Step
 
 class Snakefile(object):
    """
@@ -9,15 +10,17 @@ class Snakefile(object):
 
    The Snakefile is defined by several sections:
    - environment: {} (environment var key/value pairs to be set for the project before the build is started)
-   - build: [] (an array of steps to be run in sequence in order to execute the build)
+   - build: [] (an array of Step objects to be run in sequence in order to execute the build)
    - hooks: [] arrays of hooks to be run at various points before, during and after a build run
       - before_build: []
       - after_passing: []
       - after_failing: []
    """
 
-   def __init__(self, snakefile):
-      pass
+   def __init__(self, repo_path):
+      self.repo_path = repo_path
+      self.snakefile = path.join(self.repo_path)
+      self._parse()
 
    def _parse(self):
       pass
