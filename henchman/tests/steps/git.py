ce5fc872e571a720d66a0f60f283711edb3a5f97
Justin Morris <justin@pixelbloom.com>
2011-12-17 11:23:05 +1100

    Added placeholder git test steps.

--- henchman/tests/steps/git.py
+++ henchman/tests/steps/git.py
@@ -1,19 +1,21 @@
+from henchman.minion.lib import Git
+
 @given(u'I have a uuid for a Project Target of "{uuid}" which does not exist in the Vault')
 def step(context, uuid):
-  assert True
+  assert False
 
 @given(u'I have a uuid for a Project Target of "{uuid}" which does exist in the Vault')
 def step(context, uuid):
-  assert True
+  assert False
 
 @when(u'I tell Git to update to "{ref}"')
 def step(context, ref):
-  assert True
+  assert False
 
 @then(u'I should see the repo checked out in a directory called "{uuid}"')
 def step(context, uuid):
-  assert True
+  assert False
 
 @then(u'the current HEAD should be "{ref}"')
 def step(context, ref):
-  assert True
+  assert False
