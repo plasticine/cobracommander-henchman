1b2c8d9234c8750536b9acf78e6fa3aa5e61df6c
Justin Morris <justin@pixelbloom.com>
2012-01-02 09:12:22 +1100

    Added cleanup method after requests are completed to have django close DB connections (which are opened for each incoming request). Cleanup fires the `request_finished`signal and we let django tae care of the rest.

--- henchman/lib/wsgi.py
+++ henchman/lib/wsgi.py
@@ -1,10 +1,10 @@
 import os
-
 from werkzeug.utils import redirect
 from werkzeug.wrappers import Request, Response
 from werkzeug.routing import Map, Rule
 from werkzeug.exceptions import HTTPException, NotFound
 from jinja2 import Environment, FileSystemLoader
+from django.core.signals import request_finished
 
 class WSGIWebsocketBase(object):
     """
@@ -18,10 +18,12 @@ class WSGIWebsocketBase(object):
 
     def application(self, environ, start_response):
         """
-        Set up the response cycle
+        Set up the response cycle.
         """
         try:
-            response = self.dispatch(Request(environ))
+            request = Request(environ)
+            response = self.dispatch(request)
+            self.cleanup()
             return response(environ, start_response)
         except HTTPException, e:
             return e(environ, start_response)
@@ -35,6 +37,17 @@ class WSGIWebsocketBase(object):
         urls = self.urls.bind_to_environ(request.environ)
         endpoint, args = urls.match()
         return endpoint(self, request, **args)
+
+    def cleanup(self):
+        """
+        This feels pretty icky. Basically this needs to be here to ensure that
+        django db connections are cleanup and and properly closed after each
+        request/response cycle - which is what django does behind the scenes (
+        thus we can use django's machinery to close the connection for us here
+        by just firing the `request_finished` signal). To leave this out means
+        that we end up with an idle connection per server thread (I think).
+        """
+        request_finished.send(sender=self)
 
     def render(self, template_name, **context):
         template = self.jinja.get_template(template_name)
