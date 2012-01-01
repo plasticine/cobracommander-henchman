0722bb9f36a8016d98e305779d5785d7154f4b8e
Justin Morris <justin@pixelbloom.com>
2012-01-02 09:15:21 +1100

    Added on_subscribe methods to serialise up the build queue and send back to client connections.

--- henchman/lib/buildqueue.py
+++ henchman/lib/buildqueue.py
@@ -1,16 +1,26 @@
 import gevent
+import json
+import jsonpickle
+from django_socketio import events, broadcast_channel
+from django.utils import simplejson as json
+from .json_encoder import ModelJSONEncoder
+
 from ..minion import Minion
 
 class BuildQueue(object):
   """
   The BuildQueue class is used to manage Minions either currently being built
   or waiting to be built.
+
+  Defines socket.io methods for subscribing and updating the build queue. These
+  are wrapped up on django-socketio events.
   """
 
   def __init__(self):
     self._queue = []
     self.current_item = 0
     gevent.spawn(self.monitor_queue)
+    events.on_subscribe(channel='build_queue', handler=self._on_subscribe)
 
   def __len__(self):
     return len(self._queue)
@@ -31,7 +41,10 @@ class BuildQueue(object):
     """
     Create a new Minion instance for `id` and append it to the internal queue list.
     """
-    self._queue.append(Minion(id=id))
+    minion = Minion(id=id)
+    self._queue.append(minion)
+    data = json.dumps(self._queue, cls=ModelJSONEncoder)
+    broadcast_channel(data, 'build_queue')
 
   def monitor_queue(self):
     """
@@ -41,3 +54,10 @@ class BuildQueue(object):
       if len(self._queue) and len(filter(lambda x: x.is_active(), self._queue)) < 1:
         self._queue[0].start()
       gevent.sleep(0.5)
+
+  def _on_subscribe(self, request, socket, context, message):
+    """
+    New clients should recieve the buffer of all full queue.
+    """
+    data = json.dumps(self._queue, cls=ModelJSONEncoder)
+    socket.send(data)
