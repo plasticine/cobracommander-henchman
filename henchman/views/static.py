76d57454f6aba930887c0c842d40ff5cb9692878
Justin Morris <justin@pixelbloom.com>
2012-01-02 09:10:48 +1100

    Output some extra debugging info for channels and clients.

--- henchman/views/static.py
+++ henchman/views/static.py
@@ -1,6 +1,10 @@
 from werkzeug.wrappers import Response
+from django_socketio.channels import CHANNELS
+from django_socketio.views import CLIENTS
 
 def root(server, request):
     return server.render('index.html', **{
-      'queue':server.queue
+      'queue':    server.queue,
+      'clents':   CLIENTS,
+      'channels': CHANNELS
     })
