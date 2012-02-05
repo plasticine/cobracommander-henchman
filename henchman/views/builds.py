a5ac9e02b29cdf66133acd3514dbc1ae9d6cd6c7
Justin Morris <justin@pixelbloom.com>
2012-02-06 08:29:28 +1100

    Better docstring for the new build view.

--- henchman/views/builds.py
+++ henchman/views/builds.py
@@ -4,6 +4,12 @@ from cobracommander.apps.build.models import Build
 def new(server, request):
     """
     Accepts POST requests containing the id of the build to spawn a minion for.
+
+    If build id is not passed in the POST request, or the build id is invalid
+    (the record does not exist) then return a 400.
+
+    If the build id is valid then append the build to the build queue for
+    processing.
     """
     if request.method == 'POST':
         build_id = request.form['id']
