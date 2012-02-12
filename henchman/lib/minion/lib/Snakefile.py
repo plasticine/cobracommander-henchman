2997d59cadfa8f6bd6b649d3699b15a71d4c3407
Justin Morris <justin@pixelbloom.com>
2012-02-12 23:53:19 +1100

    TDD the snakefile class.

--- henchman/lib/minion/lib/Snakefile.py
+++ henchman/lib/minion/lib/Snakefile.py
@@ -1,66 +1,74 @@
+from django.conf import settings
 from os import path, chmod
 import subprocess
 import json
 
-from django.conf import settings
-
 from .step import Step
 
-class InvalidSnakefile(Exception):
+
+class SnakeFileNotExecutionError(Exception):
+   pass
+
+class SnakeFileValidationError(Exception):
+   pass
+
+class SnakeFileJSONError(Exception):
    pass
 
 
 class Snakefile(object):
    """
-   Handle the reading and parsing of snakefiles from within Project codebase.
+   Wrap up loading and parsing/validation of snakefiles.
 
-   A Snakefile is a JSON parseable file that tells Cobracommander how to setup
-   and build your project.
-
-   The Snakefile is defined by several sections:
-   - environment: {} (environment var key/value pairs to be set for the project before the build is started)
-   - build: [] (an array of Step objects to be run in sequence in order to execute the build)
-   - hooks: [] arrays of hooks to be run at various points before, during and after a build run
-      - before_build: []
-      - after_passing: []
-      - after_failing: []
+   The snakefile requires a path param, which will be used when
+   self.load() is called to;
+   - mark the file as executable
+   - execute the file and store the result
+   - validate that the output of the snakefile is valid and meets all requirements
    """
 
-   def __init__(self, local_path):
-      self.snakefile = None
-      self.local_path = local_path
-      self.snakefile_path = path.join(self.local_path, 'snakefile')
+   def __init__(self, path):
+      self.path = path
+      self._stderr = stderr=subprocess.PIPE
 
    def __getitem__(self, key):
-      self.read()
-      return self.snakefile[key]
+      return self._snakefile[key]
 
    def __len__(self):
-      self.read()
-      return len(self.snakefile)
+      return len(self._snakefile)
 
-   def read(self):
-      if self.snakefile == None:
-         self._contents = self._run_and_return_output()
-         self._parse()
-      return True
+   def load(self):
+      self._set_permissions()
+      self._execute()
+      self._load_json()
+      self._validate()
+      # self._wrap_build_steps()
 
-   def _run_and_return_output(self):
-      chmod(self.snakefile_path, 0777)
-      p = subprocess.Popen(self.snakefile_path, shell=True,
-               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-      snakefile, err = p.communicate()
-      if err == '':
-         print snakefile
-         return snakefile
-      raise InvalidSnakefile("Invalid Snakefile: %s", err)
+   def _set_permissions(self):
+      chmod(self.path, 0777)
 
-   def _parse(self):
-      self.snakefile = json.loads(self._contents)
+   def _execute(self):
       try:
-         assert 'build' in self.snakefile, "Your Snakefile is missing a 'build' attribute."
-         assert len(self.snakefile['build']) > 0, "You have not defined any build steps in your snakefile."
+         self._snakefile_output = subprocess.check_output(
+            self.path,
+            shell=True,
+            stderr=self._stderr
+         )
+      except Exception, e:
+         raise SnakeFileNotExecutionError(e)
+
+   def _load_json(self):
+      try:
+         self._snakefile = json.loads(self._snakefile_output)
+      except Exception, e:
+         raise SnakeFileJSONError(e)
+
+   def _validate(self):
+      try:
+         assert 'build' in self._snakefile, "Your Snakefile is missing a 'build' attribute."
+         assert len(self._snakefile['build']) > 0, "You have not defined any build steps in your snakefile."
       except AssertionError, e:
-         raise InvalidSnakefile("Invalid Snakefile: %s", e)
-      self.snakefile['build'] = map(lambda command: Step(local_path=self.local_path, command=command),
-                           self.snakefile['build'])
+         raise SnakeFileValidationError(e)
+
+   def _wrap_build_steps(self):
+      self._snakefile['build'] = map(lambda x: Step(command=x), self._snakefile['build'])
--- tests/features/snakefile.feature
+++ tests/features/snakefile.feature
@@ -1,27 +1,22 @@
 Feature: builds are defined by a file (snakefile) in the project codebase
 
-  @wip
   Scenario: Snakefile is executed and parsed into a Snakefile object
     Given that we have a valid Snakefile
-    And the Snakefile is executable
-    When the Snakefile is executed
-    Then a Snakefile object is returned
-    And the Snakefile object contains the parsed file
+    When the Snakefile is loaded
+    Then the returned Snakefile object contains the parsed file
 
   Scenario: Snakefile cannot be executed
     Given that we have a valid Snakefile
     And the Snakefile is not executable
-    When the Snakefile is executed
-    Then a SnakeFileNotExecutable exception is raised
+    When the Snakefile is loaded
+    Then a SnakeFileNotExecutionError exception is raised
 
   Scenario: Snakefile is malformed and cannot be parsed as JSON
     Given that we have a malformed Snakefile
-    And the Snakefile is executable
-    When the Snakefile is executed
+    When the Snakefile is loaded
     Then a SnakeFileJSONError exception is raised
 
   Scenario: Snakfile does not contain any build step definitions
     Given that we have an invalid Snakefile
-    And the Snakefile is executable
-    When the Snakefile is executed
+    When the Snakefile is loaded
     Then a SnakeFileValidationError exception is raised
