3ddaf9f8ee0cc92e49c370b6802acc4530fee209
Justin Morris <justin@pixelbloom.com>
2012-02-12 19:08:41 +1100

    Tests are now run inside transactions and DB-bound tests now require an '@db' tag. Faster tests are win.

--- tests/environment.py
+++ tests/environment.py
@@ -2,9 +2,10 @@ from gevent.monkey import patch_all; patch_all()
 import gevent
 from multiprocessing import Process
 
+from django import db
 from django.conf import settings
 from django.test.utils import setup_test_environment, teardown_test_environment
-from django.db import connection
+from django.core.management import call_command
 
 from henchman import setup_environment
 setup_environment()
@@ -15,17 +16,26 @@ from werkzeug.wrappers import BaseResponse
 from henchman.henchman import Henchman
 from henchman.views.socketio import SocketIOHandler
 
-interactive = True
-verbosity = 0
 old_name = settings.DATABASES['default']['NAME']
 
 def before_all(context):
     setup_test_environment()
 
 def after_all(context):
+    # clean up django test DB
+    db.close_connection()
+    db.connection.creation.destroy_test_db(old_name, verbosity=0)
     teardown_test_environment()
 
 def before_feature(context, feature):
+    if 'db' in feature.tags:
+        # set up django database
+        db.connection.creation.create_test_db(
+            verbosity=0,
+            autoclobber=False
+        )
+        db.connection.enter_transaction_management()
+
     if 'browser' in feature.tags:
         # run the Henchman server
         # we need to wrap the serve_forever call in another process here to ensure
@@ -39,14 +49,27 @@ def before_feature(context, feature):
         context.client = Client(context.henchman.application, BaseResponse)
 
 def after_feature(context, feature):
+    if 'db' in feature.tags:
+        # truncate the database between each feature
+        call_command(
+            'flush',
+            verbosity=1,
+            interactive=False,
+            database=db.connection.alias
+        )
+
     if 'browser' in feature.tags:
         # kill the server and shut down
         context.process.terminate()
 
 def before_scenario(context, scenario):
-    # set up django database
-    connection.creation.create_test_db(verbosity, autoclobber=not interactive)
+    # start transaction management for the db conenction
+    if 'db' in scenario.feature.tags:
+        db.transaction.enter_transaction_management(managed=True)
+        db.transaction.managed(True)
 
 def after_scenario(context, scenario):
-    # clean up django test DB
-    connection.creation.destroy_test_db(old_name, verbosity)
+    # rollback transactions and end transaction management
+    if 'db' in scenario.feature.tags:
+        db.transaction.rollback()
+        db.transaction.leave_transaction_management()
